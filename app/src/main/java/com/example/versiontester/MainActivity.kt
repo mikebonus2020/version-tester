14e1ca74dfe6191c2a176aed32dfa11e465d3404
MIKE <mikebonus777@gmail.com>
2022-10-05 23:37:24 -0400

    commit-2

--- app/src/main/java/com/example/versiontester/MainActivity.kt
+++ app/src/main/java/com/example/versiontester/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.versiontester.ui.theme.VersionTesterTheme
 
+// commit 2
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
